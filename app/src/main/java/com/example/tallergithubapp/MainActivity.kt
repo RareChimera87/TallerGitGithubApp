cf7b33743ff413f30a490335c3f77de4c5b0833b
RareChimera87 <sanctireinadiaz@gmail.com>
2026-09-16 15:01:15 -0500

    feat: agreaga datos personales

--- app/src/main/java/com/example/tallergithubapp/MainActivity.kt
+++ app/src/main/java/com/example/tallergithubapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +23,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,21 +48,89 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TrabajoHoy() {
     Column (modifier=Modifier
-        .background(Color.Gray)
+        .background(Color.White)
         .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround,
+        //verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
 
     ){
+        Spacer(modifier = Modifier.height(30.dp))
         Image(
             painter =
                 painterResource(R.drawable.yo),
             contentDescription = "Foto de perfil",
-            modifier = Modifier.height(150.dp)
+            modifier = Modifier.height(120.dp)
                 .clip(CircleShape)
         )
         Spacer(modifier = Modifier.height(25.dp))
-        
+        Text(
+            text = "Santiago Reina",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            color = Color.Black
+        )
+        Text(
+            text = "Estudiante Ing. Sistemas",
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            color = Color.Blue
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        HorizontalDivider()
+        Column(modifier=Modifier
+            .padding(10.dp)
+            .fillMaxSize(),
+            //verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.Start
+
+
+        ) {
+            Text(
+                text = "EDAD",
+                fontSize = 20.sp,
+                color = Color.Gray,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            )
+            Text(
+                text = "19",
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            )
+            Text(
+                text = "Correo",
+                fontSize = 20.sp,
+                color = Color.Gray,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            )
+            Text(
+                text = "sreina63@unab.edu.co",
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            )
+            Text(
+                text = "Ciudad",
+                fontSize = 20.sp,
+                color = Color.Gray,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            )
+            Text(
+                text = "Bucaramanga, Colombia",
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            )
+        }
+        HorizontalDivider()
+
 
     }
 
